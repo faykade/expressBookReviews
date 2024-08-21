0fb07d9f1a9695793d2fba24b4c08492042c66df
Thomas Ulrich <ulrthomas@gmail.com>
2024-08-20 22:08:27 -0500

    finishes up the lab

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,8 +1,14 @@
 const express = require("express");
-let books = require("./booksdb.js");
+let booksSync = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+
+const getBooks = async () => {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(booksSync), 1000);
+  });
+};
 
 public_users.post("/register", (req, res) => {
   const username = req.body.username;
@@ -23,13 +29,14 @@ public_users.post("/register", (req, res) => {
 });
 
 // Get the book list available in the shop
-public_users.get("/", function (req, res) {
+public_users.get("/", async function (req, res) {
+  const books = await getBooks();
   return res.json(books);
 });
 
 // Get book details based on ISBN
-public_users.get("/isbn/:isbn", function (req, res) {
-  //Write your code here
+public_users.get("/isbn/:isbn", async function (req, res) {
+  const books = await getBooks();
   const book = books[req.params.isbn];
   if (book) {
     return res.json(book);
@@ -39,7 +46,8 @@ public_users.get("/isbn/:isbn", function (req, res) {
 });
 
 // Get book details based on author
-public_users.get("/author/:author", function (req, res) {
+public_users.get("/author/:author", async function (req, res) {
+  const books = await getBooks();
   const ret = {};
   Object.keys(books).forEach((key) => {
     if (books[key].author === req.params.author) {
@@ -49,8 +57,9 @@ public_users.get("/author/:author", function (req, res) {
   return res.json(ret);
 });
 
-// Get all books based on title
-public_users.get("/title/:title", function (req, res) {
+// Get all booksSync based on title
+public_users.get("/title/:title", async function (req, res) {
+  const books = await getBooks();
   const ret = {};
   Object.keys(books).forEach((key) => {
     if (books[key].title === req.params.title) {
@@ -61,7 +70,8 @@ public_users.get("/title/:title", function (req, res) {
 });
 
 //  Get book review
-public_users.get("/review/:isbn", function (req, res) {
+public_users.get("/review/:isbn", async function (req, res) {
+  const books = await getBooks();
   const book = books[req.params.isbn];
   if (book) {
     return res.json(book.reviews);
